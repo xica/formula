3f38d2c6e22cd5d2d71fd9fa794bf772033b83aa
Fujimoto Seiji <fujimoto@ceptord.net>
2016-07-27 18:06:20 +0900

    Write the setup script.
    Now you can install the formula module via:
    
        $ python setup.py install

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name='formula',
+    version='0.1.0',
+    py_modules=['formula'],
+    author='XICA.inc',
+    author_email='info@xica.net',
+    url='https://github.com/xica/formula',
+    description='Safe arithmetic expression evaluator',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ]
+)
