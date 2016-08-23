e1265fdaf996143f67abd7dec70f36a3bacd9611
Fujimoto Seiji <fujimoto@ceptord.net>
2016-08-23 15:35:45 +0900

    Formula 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='formula',
-    version='0.1.0',
+    version='0.2.0',
     py_modules=['formula'],
     author='XICA.inc',
     author_email='info@xica.net',
