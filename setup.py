61878feaaf2088554c2da151a7281f736f6990f8
Fujimoto Seiji <fujimoto@ceptord.net>
2016-08-23 15:59:34 +0900

    Update author information.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='formula',
     version='0.2.0',
     py_modules=['formula'],
-    author='XICA.inc',
+    author='XICA',
     author_email='info@xica.net',
     url='https://github.com/xica/formula',
     description='Safe arithmetic expression evaluator',
