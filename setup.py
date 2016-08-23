2bc65732d63fa5be1d3defed2b153c529f40690f
Fujimoto Seiji <fujimoto@ceptord.net>
2016-08-23 18:30:43 +0900

    Add 'long_description' to setup.py.
    This description is just an except from README.md.

--- setup.py
+++ setup.py
@@ -8,6 +8,11 @@ setup(
     author_email='info@xica.net',
     url='https://github.com/xica/formula',
     description='Safe arithmetic expression evaluator',
+    long_description="""Formula is a simple arithmetic expression evaluator written in Python.
+The main goal of this library is to provide a safer way to evaluate
+basic math expressions given in string form.
+Internally, it implements a (simplified) version of Dijkstra's "shunting-
+yard algorithm".""",
     license='MIT',
     classifiers=[
         'Development Status :: 3 - Alpha',
